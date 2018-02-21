6b68d5b0fee7c5b6473577ceb85f16760de53a70
icystem <connie_fu@yahoo.com>
2018-02-20 20:26:30 -0500

    Initial version of the entire code.

---
+++ Mad_Libs_2.py
@@ -0,0 +1,39 @@
+story = '''
+{}. But! {} only if ye be a {} that smells!
+For {} is guarded by a {} so {},
+so {}, that no {} has yet {}
+it... and {}!
+'''
+
+def main():
+    command = input('Enter a command:')
+    plural_noun = input('Enter a plural noun:')
+    animal = input('Enter an animal:')
+    location = input('Enter a location:')
+    singular_noun = input('Enter a singular noun:')
+
+    adjectives = []
+    adjectives.append(input('Enter an adjective:'))
+    adjectives.append(input('Enter another adjective:'))
+
+
+    past_participles = []
+    past_participles.append(input('Enter a past participle:'))
+    past_participles.append(input('Enter another past participle:'))
+
+    mad_lib = story.format(command,
+                           command,
+                           plural_noun,
+                           location,
+                           animal,
+                           adjectives[0],
+                           adjectives[1],
+                           singular_noun,
+                           past_participles[0],
+                           past_participles[1])
+    print(mad_lib)
+
+    #print(past_participles[0], past_participles[1])
+
+
+main()
